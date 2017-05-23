fe493c07ca4493fdcceb436dcdcd3ee400faf9bc
Grigory <krasilnikov@phystech.edu>
2017-05-23 12:21:55 +0300

    Filter

--- Homework2/homework1-master/src/main/java/ru/sbt/bit/ood/hw1/Downloader.java
+++
@@ -1,9 +0,0 @@
-package ru.sbt.bit.ood.hw1;
-
-/**
- * Created by Григорий on 20.12.2016.
- */
-
-public interface Downloader{
-    public String downloadTradesFile();
-}
---
+++ gateway/src/main/java/ru/sbt/ds/PhotoServiceFilter.java
@@ -0,0 +1,73 @@
+package ru.sbt.ds;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
+import com.netflix.discovery.shared.Applications;
+import com.netflix.loadbalancer.IPing;
+import com.netflix.loadbalancer.PingUrl;
+import com.netflix.loadbalancer.Server;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.net.URL;
+import java.util.Comparator;
+
+
+@Component
+public class PhotoServiceFilter extends ZuulFilter {
+
+    @Autowired
+    private EurekaClient discoveryClient;
+
+    PingUrl pinger = new PingUrl();
+
+    @Override
+    public String filterType() {
+        return "route";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+
+
+    @Override
+    public Object run() {
+        Applications apps = discoveryClient.getApplications();
+        Application application = null;
+        application = apps.getRegisteredApplications()
+                .stream()
+                .filter(s -> s.getName().toLowerCase().equals("photo-service"))
+                .findFirst()
+                .get();
+
+        pinger.setSecure(false);
+        pinger.setPingAppendString("/img?id=100");
+
+        InstanceInfo target = application.getInstances()
+                .stream()
+                .sorted(Comparator.comparingLong(s -> Long.parseLong(s.getMetadata().get("version"))))
+                .filter(s -> pinger.isAlive(new Server(s.getHostName(), s.getPort())))
+                .findFirst()
+                .get();
+
+        RequestContext ctx = RequestContext.getCurrentContext();
+        try {
+            ctx.setRouteHost(new URL(target.getHomePageUrl()));
+        } catch (Throwable e){
+            System.out.println("Error!");
+        }
+        return null;
+    }
+
+}
